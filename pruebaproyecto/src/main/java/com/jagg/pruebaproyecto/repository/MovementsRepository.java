acd5e4b31783ce84863ccb382c6f1be683e43d67
Ingjaguar <ing.jaguarin@gmail.com>
2022-10-30 10:35:09 -0500

    Error

--- pruebaproyecto/src/main/java/com/jagg/pruebaproyecto/repository/MovementsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jagg.pruebaproyecto.repository;
-
-import com.jagg.pruebaproyecto.entities.Movements;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovementsRepository extends JpaRepository<Movements, Long> {
-}
